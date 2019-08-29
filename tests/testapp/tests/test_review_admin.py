ebcfed5c01a4f890f4d6ba09646f7e48019c3c9c
Andrey Novikov <novikov@gmail.com>
2019-08-29 18:19:12 +0300

    Add some admin tests

---
+++ tests/testapp/tests/test_review_admin.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.conf import settings
+from django.contrib.admin.sites import AdminSite
+from django.contrib.auth.models import User
+from django.test import RequestFactory
+from django.test.utils import override_settings
+
+
+from reviews.admin import ReviewAdmin
+from reviews.models import Review
+from reviews.widgets import ObjectPkWidget
+
+from . import ReviewTestCase
+
+
+class MockSuperUser:
+    def has_perm(self, perm):
+        return True
+
+
+@override_settings(ROOT_URLCONF='testapp.urls_admin')
+class ReviewAdminTests(ReviewTestCase):
+
+    def setUp(self):
+        super(ReviewAdminTests, self).setUp()
+        site = AdminSite()
+        self.admin = ReviewAdmin(Review, site)
+        request_factory = RequestFactory()
+        self.request = request_factory.get('/admin')
+        self.request.user = MockSuperUser()
+
+    def testDeleteReview(self):
+        self.createSomeReviews()
+        review = Review.objects.get(pk=1)
+        self.admin.delete_model(self.request, review)
+        deleted = Review.objects.filter(pk=1).first()
+        self.assertEqual(deleted, None)
+
+    def testObjectPkWidget(self):
+        self.createSomeReviews()
+        widget = ObjectPkWidget(Review.objects.get(pk=1))
+        html = widget.render('object_pk', 1, attrs={})
+        self.assertHTMLEqual(html, '<input type="text" name="object_pk" value="1">&nbsp;&nbsp;<strong><a href="/admin/testapp/article/1/change/">Man Bites Dog</a></strong>')
--- tests/testapp/urls_admin.py
+++ tests/testapp/urls_admin.py
@@ -1,19 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django_comments.admin import CommentsAdmin
-from django_comments.models import Comment
+from reviews.admin import ReviewAdmin
+from reviews.models import Review
 
-# Make a new AdminSite to avoid picking up the deliberately broken admin
-# modules in other tests.
+from testapp.models import Article
+
+
 admin_site = admin.AdminSite()
-admin_site.register(Comment, CommentsAdmin)
-
-# To demonstrate proper functionality even when ``delete_selected`` is removed.
-admin_site2 = admin.AdminSite()
-admin_site2.disable_action('delete_selected')
-admin_site2.register(Comment, CommentsAdmin)
+admin_site.register(Review, ReviewAdmin)
+admin_site.register(Article)
 
 urlpatterns = [
     url(r'^admin/', admin_site.urls),
-    url(r'^admin2/', admin_site2.urls),
 ]

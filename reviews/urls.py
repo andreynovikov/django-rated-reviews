dcae5130b7ef27e8e9ff3ab5c380f0906ff6f470
Bo Peng <ben.bob@gmail.com>
2021-04-13 17:42:14 -0500

    Fix python3 compatibility

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 from django.contrib.contenttypes.views import shortcut
 
 from .views import post_review, review_done
 
 
 urlpatterns = [
-    url(r'^post/$', post_review, name='post-review'),
-    url(r'^posted/$', review_done, name='review-done'),
-    url(r'^rr/(\d+)/(.+)/$', shortcut, name='review-url-redirect'),
+    re_path(r'^post/$', post_review, name='post-review'),
+    re_path(r'^posted/$', review_done, name='review-done'),
+    re_path(r'^rr/(\d+)/(.+)/$', shortcut, name='review-url-redirect'),
 ]

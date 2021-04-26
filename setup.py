ccb24f5412bf5c831f79120c32e1cd51ddca5fe8
Andrey Novikov <novikov@gmail.com>
2021-04-26 18:21:50 +0300

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='django-rated-reviews',
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     author='Andrey Novikov',
     author_email='novikov@gmail.com',
